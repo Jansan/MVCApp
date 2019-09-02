db31c97294e9d9f7b502ebe89a8c5252675a223d
Jansan <jansan-26@hotmail.com>
2019-09-02 12:28:27 +0200

    initial commit

---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        [Range(100000, 999999, ErrorMessage = "You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string ConfirmEmail { get; set; }
+        public string Password { get; set; }
+        public string ConformPassword { get; set; }
+
+
+
+    }
+}
