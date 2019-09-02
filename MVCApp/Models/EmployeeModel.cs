ce49c034c16ab5aadf900f96d793fb3830bc8985
Jansan <jansan-26@hotmail.com>
2019-09-02 14:44:07 +0200

    Created DataLibrary Models, BusinessLogic, DataAccess folde

--- MVCApp/Models/EmployeeModel.cs
+++ MVCApp/Models/EmployeeModel.cs
@@ -8,13 +8,36 @@ namespace MVCApp.Models
 {
     public class EmployeeModel
     {
+        [Display(Name ="Employee ID")]
         [Range(100000, 999999, ErrorMessage = "You need to enter a valid EmployeeId")]
         public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="You need to give us your first name.")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to give us your last name.")]
         public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to give us your email address.")]
         public string EmailAddress { get; set; }
+
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress", ErrorMessage = "The Email and Confirm Email must match.")]
         public string ConfirmEmail { get; set; }
+
+        [Display(Name ="Password")]
+        [Required(ErrorMessage ="You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 10, ErrorMessage ="You need to provide a long enough password.")]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The Password and Confirm Password must match.")]
         public string ConformPassword { get; set; }
 
 
